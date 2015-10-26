e654d453b89d632b02ca20dd7e96212b62284f55
ScottAndJake <ScottAndJake@appacademy.io>
2015-10-26 11:01:47 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoalSetterApp::Application.config.secret_key_base = 'b9461f7197b7ed57b4512dd05eab86ca3d1b34a80567b3b6e4052bd5eeff1ae1bd31de89fc30f059dd2648d068b041323d702424327c8916d66a46f6f25b7d2c'
